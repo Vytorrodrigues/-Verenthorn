794123aafcc54653fca28abc28bd554daf35c8f2
unknown <vitugasbriel@gmail.com>
2026-04-23 10:16:36 -0300

    feat new logic

--- main.js
+++ main.js
@@ -1,142 +1,140 @@
-let campGames = document.getElementById("jogos");
-let campWin = document.getElementById("vitorias");
-let campPlayer = document.getElementById("player");
-let campCPU = document.getElementById("cpu");
-let campResult = document.getElementById("result");
-let pedra = document.getElementById("pedra");
-let papel = document.getElementById("papel");
-let tesoura = document.getElementById("tesoura");
+const deckOopente = document.getElementById("deckO");
+const deckJogadorc = document.getElementById("deckJ");
 
-let deckGlobal =[];
+const zonaCentral = document.getElementById("centralZ");
 
-// //CHAMADA A API
-const cardsJSON = async () => {
+const cemiterioOponente = document.getElementById("cemiO");
+const cemiterioJogador = document.getElementById("cemiJ");
 
-    try {
-        const response = await fetch("./cards.json");
+const battles = document.getElementById("battles");
+const wins = document.getElementById("wins");
+const defeat = document.getElementById("defeats");
 
-        if (!response.ok){
-            throw new Error(`Erro ao carregar dados ${response.status}`);
+const cardCPU1 = document.getElementById("cardCPU1");
+const cardCPU2 = document.getElementById("cardCPU2");
+const cardCPU3 = document.getElementById("cardCPU3");
+
+const cardPlayer1 = document.getElementById("cardPlayer1");
+const cardPlayer2 = document.getElementById("cardPlayer2");
+const cardPlayer3 = document.getElementById("cardPlayer3");
+
+
+class JokenpoGame{
+    //definindo atributos
+    constructor(jsonURL){
+        this.jsonURL = jsonURL;
+        this.allCards = [];
+        this.rules = {
+            pedra: "tesoura",
+            tesoura: "papel",
+            papel: "pedra"
+        };
+    };
+
+    //chamada API metodo
+    async loadCards(){
+        try {
+            const response = await fetch(this.jsonURL);
+
+            const data = await response.json();
+
+            this.allCards = data.cards;
+            console.log("Cards read");
+        }catch(error){
+            console.log("Erro on load JSON", error);
+        };
+    };
+
+    //SORTEIO Carta metodo
+    getRandonCard(amount){
+        return [...this.allCards].sort(() => Math.random() - 0.5).slice(0,amount);
+    };
+
+    //criar elemento html metodo
+    createCard(card){
+        return `
+            <div class="card">
+                <!--<img src="${card.image}" alt="${card.name}" class="cardImage"> --!>
+                 <div class="card-info">
+                     <h2>${card.name}</h2>
+                     <p>Tipo: ${card.type}</p>
+                     <span class="id-tag">#${card.id}</span>
+                 </div>
+             </div>
+        `;
+    };
+
+    //metodo para iniciar
+    async play(){
+        if(this.allCards.length === 0){
+            await this.loadCards();
         };
 
-        const data = await response.json();
-        return data;
+        const sortCard = this.getRandonCard(6);
+        const playerHand = sortCard.slice(0, 3);
+        const cpuHand = sortCard.slice(3, 6);
 
-    } catch (error) {
-      console.log(`Erro na requisição das cartas`, error);
-    } finally  {
-        console.log("Requisition ended");
+        const result = this.battle(playerHand, cpuHand);
+
+        const campPlayer = document.getElementById("campPlayer");
+        const card = document.querySelector(".card");
+        
+        if ( campPlayer) {
+            campPlayer.innerHTML = playerHand.map((card) => this.createCard(card)).join("");
+        } else {
+            alert("Carta não encontrado");
+        };
+
+        const info = document.getElementById('gameInfo');
+        info.innerHTML = `
+            <h2>Ganhador da Rodada: ${result.winner}</h2>
+            <p>Placar: ${result.playerPoints} vs ${result.cpuPoints}</p>
+        `;
+        
+        console.log("Detalhes da Batalha:", result.logs);
     };
 
-};
 
-class Player{
-    constructor(){
-        this.cardActual = null;
-    };
+    battle(playerHand, cpuHand){
+        let playerPoints = 0;
+        let cpuPoints = 0;
+        const logs = [];
 
-    getCardType(typeWanted, deck){
-        const cardType = deck.cards.filter(cards => cards.tipo === typeWanted);
-        let randomIndice = Math.floor(Math.random() * cardType.length);
-        this.cardActual = cardType[randomIndice];
-        return this.cardActual;
-    };
-};
+        for(let i = 0; i < 3; i++){
+            const pCard = playerHand[i];
+            const cCard = cpuHand[i];
 
-class CPU{
-    constructor(){
-        this.cardActual = null;
-    };
+            const pType = pCard.type.toLowerCase();
+            const cType = cCard.type.toLowerCase();
 
-    getCardRandom(deck){
-        let randomIndice = Math.floor(Math.random() * deck.cards.length);
-        this.cardActual = deck.cards[randomIndice];
-        return this.cardActual;
+            let resultMessage = "";
 
-    };
-};
-
-class Game{
-    constructor(){
-        this.games = 0;
-        this.wins = 0;
-    };
-
-    draw(){
-        return "DRAW";
-    };
-
-    playerWinner(){
-        this.wins += 1;
-        return "Your Card WIN!";
-    };
-
-    defeat(){
-        return "DEFEAT! The CPU beat your card.";
-    };
-
-    duel(cardPlayer, cardCPU){
-        this.games += 1;
-
-        let typePlayer = cardPlayer.type;
-        let typeCPU = cardCPU.type;
-
-        if (typePlayer === typeCPU) {
-            return this.draw();
-        } else if((typePlayer === "pedra" && typeCPU === "tesoura") ||
-            (typePlayer === "tesoura" && typeCPU === "papel") || (typePlayer === "papel" && typeCPU === "pedra")){
-                return this.playerWinner();
-            }else {
-                return this.defeat()
+            if (pType === cType){
+                resultMessage = "Empate";
+            } else if( this.rules[pType] === cType){
+                playerPoints++;
+                resultMessage = "Jogador Ganhou";
+            }else{
+                cpuPoints++;
+                resultMessage = "Vitória da CPU";
             };
-    };
 
-};
+            logs.push({
+                round: i + 1,
+                pCard: pCard.name,
+                cCard: cCard.name,
+                result: resultMessage
+            });
 
-const player1 = new Player();
-const cpu = new CPU();
-const match = new Game();
+            return {
+                playerPoints,
+                cpuPoints,
+                winner: playerPoints > cpuPoints? "Jogador" : cpuPoints > playerPoints ? "CPU" : "Empate",
+                logs
+            };
+        }
+    }
+}
 
-function relizeTurn(typeSelected){
-    if(deckGlobal.cards){
-        //ver qual mensagem sera exibida ao escolher
-        console.log("O deck está sendo invocado");
-        return;
-    };
-
-    let cardOfPlayer = player1.getCardType(typeSelected, deckGlobal);
-
-    let cardOfCPU = cpu.getCardRandom(deckGlobal);
-
-    let resultDuel = match.duel(cardOfPlayer, cardOfCPU);
-
-    //DOM
-    campResult.textContent = resultDuel;
-    //nome das cartas
-    campPlayer.textContent = cardOfPlayer.name;
-    campCPU.textContent = cardOfCPU.name;
-    
-    campGames.textContent = match.games;
-    campWin.textContent = match.wins;
-
-    //implementar imagens
-    //imagemdecks
-    //imagens cartas
-};
-
-async function initGame() {
-    
-    deckGlobal = await cardsJSON();
-    if(deckGlobal && deckGlobal.cards){
-        console.log(`${deckGlobal.length} cartas no deck`);
-    };
-
-    // papel.onclick = () => relizeTurn("papel");
-    // pedra.onclick = () => relizeTurn("pedra");
-    // tesoura.onclick = () => relizeTurn("tesoura");
-
-    //prepara front e setar seletores de cartas/decks
-};
-
-initGame();
-
+const game = new JokenpoGame(`./cards.json`);
+game.play();
